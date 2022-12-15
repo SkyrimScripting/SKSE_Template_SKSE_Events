e523927b87add1ae25c9fb479a7a87732578ff76
Mrowr Purr <mrowr.purr@gmail.com>
2022-12-15 00:14:36 -0800

    Add more informative descriptions about each event

--- plugin.cpp
+++ plugin.cpp
@@ -20,35 +20,44 @@ void SetupLog() {
 // For every message sent to the plugin by SKSE, log the message.
 void MessageListener(SKSE::MessagingInterface::Message* message) {
     switch (message->type) {
+        // Descriptions are taken from the original skse64 library
+        // See:
+        // https://github.com/ianpatt/skse64/blob/09f520a2433747f33ae7d7c15b1164ca198932c3/skse64/PluginAPI.h#L193-L212
         case SKSE::MessagingInterface::kPostLoad:
-            logger::info("POST LOAD");
+            logger::info("kPostLoad: sent to registered plugins once all plugins have been loaded");
             break;
         case SKSE::MessagingInterface::kPostPostLoad:
-            logger::info("POST POST LOAD");
+            logger::info(
+                "kPostPostLoad: sent right after kPostLoad to facilitate the correct dispatching/registering of "
+                "messages/listeners");
             break;
         case SKSE::MessagingInterface::kPreLoadGame:
-            logger::info("PRE LOAD GAME");
+            // message->dataLen: length of file path, data: char* file path of .ess savegame file
+            logger::info("kPreLoadGame: sent immediately before savegame is read");
             break;
         case SKSE::MessagingInterface::kPostLoadGame:
-            logger::info("POST LOAD GAME");
+            // You will probably want to handle this event if your plugin uses a Preload callback
+            // as there is a chance that after that callback is invoked the game will encounter an error
+            // while loading the saved game (eg. corrupted save) which may require you to reset some of your
+            // plugin state.
+            logger::info("kPostLoadGame: sent after an attempt to load a saved game has finished");
             break;
         case SKSE::MessagingInterface::kSaveGame:
-            logger::info("SAVE GAME");
+            logger::info("kSaveGame");
             break;
         case SKSE::MessagingInterface::kDeleteGame:
-            logger::info("DELETE GAME");
+            // message->dataLen: length of file path, data: char* file path of .ess savegame file
+            logger::info("kDeleteGame: sent right before deleting the .skse cosave and the .ess save");
             break;
         case SKSE::MessagingInterface::kInputLoaded:
-            logger::info("INPUT LOADED");
+            logger::info("kInputLoaded: sent right after game input is loaded, right before the main menu initializes");
             break;
         case SKSE::MessagingInterface::kNewGame:
-            logger::info("NEW GAME");
+            // message-data: CharGen TESQuest pointer (Note: I haven't confirmed the usefulness of this yet!)
+            logger::info("kNewGame: sent after a new game is created, before the game has loaded");
             break;
         case SKSE::MessagingInterface::kDataLoaded:
-            logger::info("DATA LOADED");
-            break;
-        case SKSE::MessagingInterface::kTotal:
-            logger::info("TOTAL");
+            logger::info("kDataLoaded: sent after the data handler has loaded all its forms");
             break;
         default:
             logger::info("Unknown system message of type: {}", message->type);
